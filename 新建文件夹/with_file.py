e804af5b165b42476df5e5825cd70aa834355f51
LAPTOP-680CCAL5\冯智明 <f1501578132@163.com>
2022-07-04 00:48:58 +0800

    ..

--- 新建文件夹/with_file.py
+++ 新建文件夹/with_file.py
@@ -1,4 +1,5 @@
-
+# with语句（上下文管理器）
+# with 语句可以自动管理上下文资源，不论什么原有跳出with块，都能确保文件正确的关闭看，以此达到释放资源的目的
 with open("1656861427454.jpg","rb") as file:
     with open("copy2@1656861427454.jpg","wb") as copy_file:
         copy_file.write(file.read())
