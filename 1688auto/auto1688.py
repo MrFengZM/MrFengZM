796ef2acaa50b1384bd76069f280fa24cf0675e8
MrFengZM <f1501578132@163.com>
2024-08-20 22:12:47 +0800

    1688jiaoben

--- 1688auto/auto1688.py
+++ 1688auto/auto1688.py
@@ -6,7 +6,8 @@ import time
 from thefuzz import fuzz
 from selenium.webdriver.common.by import By
 import pandas as pd
-
+import threading
+from datetime import datetime
 
 # import TouchAction
 # 初始化参数
@@ -26,9 +27,12 @@ import pandas as pd
 #     "skip_server_installation":True
 # }
 
-class Auto_1688:
+class Auto_1688(threading.Thread):
+
     def __init__(self, platform_name, platform_version, device_name, app_package, app_activity, new_command_timeout,
-                 automation_name='UiAutomator2', skip_server_installation=True, reset_keyboard=True, no_reset=True):
+                 automation_name='UiAutomator2', skip_server_installation=True, reset_keyboard=True, no_reset=True,
+                 port="4723"):
+        super().__init__()
         self.platformName = platform_name
         self.platformVersion = platform_version
         self.deviceName = device_name
@@ -39,6 +43,7 @@ class Auto_1688:
         self.newCommandTimeout = new_command_timeout
         self.automationName = automation_name
         self.skipServerInstallation = skip_server_installation
+        self.port=port
         self.desired_caps = {
             'platformName': self.platformName,  # 被测手机是安卓
             'platformVersion': self.platformVersion,  # 手机安卓版本
@@ -52,8 +57,12 @@ class Auto_1688:
             'automationName': self.automationName,
             "skipServerInstallation": self.skipServerInstallation
         }
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
+        self.driver = webdriver.Remote(f'http://localhost:{self.port}/wd/hub', self.desired_caps)
         time.sleep(3)
+
+    @staticmethod
+    def date_time():
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def like_commodity(self, commodity="yry"):
         """匹配商品名称"""
@@ -65,10 +74,10 @@ class Auto_1688:
             try:
                 a = element.text
             except Exception as a:
-                print(a, "找不到元素文本")
+                print(self.date_time(), "{}-{}-{}找不到元素文本".format(self.deviceName, self.platformVersion, self.port), a)
                 continue
             if fuzz.partial_ratio(str(element.text), commodity) == 100:
-                print("匹配成功！", element.text)
+                print(self.date_time(),"{}-{}-{}匹配成功！".format(self.deviceName,self.platformVersion,self.port), element.text)
                 element_list.append(element)
         return element_list
 
@@ -78,12 +87,12 @@ class Auto_1688:
         self.driver.tap(positions=[(400, 380)], duration=500)
         # time.sleep(3)
         # self.driver.find_element(by=By.ID,value="com.alibaba.wireless:id/content").click()
-        print("点击搜索框成功")
+        print(self.date_time(),"{}-{}-{}点击搜索框成功".format(self.deviceName,self.platformVersion,self.port))
         # self.driver.implicitly_wait(10)
 
     def search_commodity(self, commodity="yry"):
         time.sleep(1)
-        print(commodity)
+        print(self.date_time(),'{}-{}-{}'.format(self.deviceName,self.platformVersion,self.port),commodity)
         self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/et_search_input_edit").send_keys(commodity)
         self.driver.implicitly_wait(10)
         time.sleep(3)
@@ -164,7 +173,7 @@ class Auto_1688:
                 time.sleep(1)
                 self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/v5_common_return").click()
             except Exception as a:
-                print("执行返回商品列表失败", a)
+                print(self.date_time(),"{}-{}-{}执行返回商品列表失败".format(self.deviceName,self.platformVersion,self.port), a)
                 self.driver.tap(positions=[(540, 300)], duration=500)
                 time.sleep(2)
                 self.driver.implicitly_wait(10)
@@ -183,7 +192,7 @@ class Auto_1688:
             time.sleep(1)
             self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/v5_common_return").click()  # 返回商品列表
             self.driver.implicitly_wait(10)
-            print('执行加购成功!!!')
+            print(self.date_time(),'{}-{}-{}执行加购成功!!!'.format(self.deviceName,self.platformVersion,self.port))
             time.sleep(1)
 
     def view_commodity(self, element_list, is_car=0):
@@ -192,12 +201,12 @@ class Auto_1688:
             try:
                 element.click()
             except Exception as e:
-                print(e, "找不到要点击的商品")
+                print(self.date_time(),"{}-{}-{}找不到要点击的商品".format(self.deviceName,self.platformVersion,self.port),e)
                 continue
             try:
                 self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/play_icon").click()
             except Exception as e:
-                print(e, "找不到视频")
+                print(self.date_time(),"{}-{}-{}找不到视频".format(self.deviceName,self.platformVersion,self.port),e)
             else:
                 time.sleep(10)  # 播放视频10s
 
@@ -206,7 +215,7 @@ class Auto_1688:
                 try:
                     print(el.text)
                 except Exception as a:
-                    print(a, "找不到元素文本")
+                    print(self.date_time(),a, "找不到元素文本")
                     continue
                 if fuzz.partial_ratio(str(el.text), "图片") == 100:
                     el.click()
@@ -214,9 +223,9 @@ class Auto_1688:
             try:
                 text = self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/tv_index").text.split("/")
             except Exception as a:
-                print(a, '获取图片数量失败')
+                print(self.date_time(),a, '获取图片数量失败')
                 text = 4
-            for i in int(text) - 1:
+            for i in range(int(len(text)) - 1):
                 self.driver.swipe(start_x=960, start_y=515, end_x=100, end_y=515)
                 self.driver.implicitly_wait(10)
                 time.sleep(2)
@@ -225,7 +234,7 @@ class Auto_1688:
             self.driver.implicitly_wait(10)
             time.sleep(1)
             if is_car == 1:
-                print("开始执行加购行为")
+                print(self.date_time(),"开始执行加购行为")
                 self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/action_title").click()  # 点击加购
                 self.driver.implicitly_wait(10)
                 self.driver.find_element(by=By.ID,
@@ -233,20 +242,21 @@ class Auto_1688:
                 self.driver.implicitly_wait(10)
                 self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/sku_confirm").click()  # 确认
                 self.driver.implicitly_wait(10)
-                print("加购完成")
+                print(self.date_time(),"加购完成")
                 self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/od_title_bar_back_btn").click()
 
 
             else:
-                print("跳过加购,返回搜索页")
+                print(self.date_time(),"跳过加购,返回搜索页")
                 self.driver.find_element(by=By.ID, value="com.alibaba.wireless:id/od_title_bar_back_btn").click()
 
     def down_glide(self):
         self.__slide(self.y_view(0), self.y_view(1), self.y_view(2), 2)
 
 
-def main(commodity):
-    dv = Auto_1688('Android', '9', 'android666', 'com.alibaba.wireless', 'launch.LauncherActivity', 6000)
+def main(commodity, port="4723", version="9", name='android666'):
+    dv = Auto_1688('Android', f'{version}', f'{name}', 'com.alibaba.wireless', 'launch.LauncherActivity', 6000,
+                   port=port)
 
     """点击搜索框"""
     for i in range(3):
@@ -254,12 +264,12 @@ def main(commodity):
             dv.search_clink()
             time.sleep(1)
         except Exception as e:
-            print(e,"点击搜索框失败")
+            print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),"点击搜索框失败",e)
             continue
         else:
             break
 
-    print("遍历商品")
+    print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),"遍历商品")
     for cdy in commodity:
         """搜索商品"""
         # time.sleep(1)
@@ -271,33 +281,34 @@ def main(commodity):
             for i in range(10):
                 elements = dv.like_commodity("中山市宜润元电子商务商行")
                 if not elements:
-                    print("此页面没有匹配到商品，继续滑动")
+                    print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),"此页面没有匹配到商品，继续滑动")
                     time.sleep(0.5)
                     dv.down_glide()
                     time.sleep(0.5)
                     dv.down_glide()
                 else:
-                    print(f"成功匹配到{len(elements)}个商品,执行浏览加购业务")
+                    print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),f"成功匹配到{len(elements)}个商品,执行浏览加购业务")
                     for element in elements:
                         time.sleep(2)
                         dv.view_commodity_old(element, 1)
                     break
         except Exception as a:
-            print(a,cdy,"{匹配商品异常}")
+            print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),a, cdy, "{匹配商品异常}")
         try:
             time.sleep(2)
             dv.return_commodity()
             time.sleep(1)
         except Exception as a:
-            print("执行清除失败，开始重启app")
-            dv = Auto_1688('Android', '9', 'android666', 'com.alibaba.wireless', 'launch.LauncherActivity', 6000)
+            print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),"执行清除失败，开始重启app")
+            dv = Auto_1688('Android', '9', 'android666', 'com.alibaba.wireless', 'launch.LauncherActivity', 6000,
+                           port=port)
             """点击搜索框"""
             for i in range(3):
                 try:
                     dv.search_clink()
                     time.sleep(1)
                 except Exception as e:
-                    print(e, "点击搜索框失败")
+                    print(Auto_1688.date_time(),'{}-{}-{}'.format(name,version,port),e, "点击搜索框失败")
                     continue
                 else:
                     break
--- 1688auto/main.py
+++ 1688auto/main.py
@@ -1,25 +1,42 @@
 import numpy as np
 
 import auto1688
-
+import threading
 import pandas
 import numpy
 from enum import Enum
-
+from datetime import datetime
+import time
 from filereader import DataFrameReader
 
 
 class file_info(Enum):
-    PATH = "C:\\Users\\admin\\Downloads\\SPU_2024_08_19_01_38_39.xlsx"
+    PATH = "C:\\Users\\admin\\Downloads\\SPU_2024_08_20_09_02_24.xlsx"
     SHEET = "Sheet1"
     COL = 1
 
 
+class MyThread(threading.Thread):
+    def __init__(self, port, version, name):
+        super().__init__()
+        self.port = port
+        self.version = version
+        self.name = name
+        self.commodity_list = DataFrameReader(file_info.PATH.value, file_info.SHEET.value,
+                                              file_info.COL.value).read_data()
+
+    def run(self) -> None:
+        while True:
+            try:
+                print('{} is running >> {}'.format(threading.current_thread().name,datetime.now().strftime("%Y-%m-%d "
+                                                                                                           "%H:%M:%S")))
+                auto1688.main(self.commodity_list, self.port, self.version, self.name)
+            except Exception as a:
+                print('{}-{}-{}'.format(self.name,self.version,self.port),"Error:运行设备异常", a)
+                continue
+
+
 if __name__ == "__main__":
 
-    commodity_list = DataFrameReader(file_info.PATH.value, file_info.SHEET.value, file_info.COL.value).read_data()
-    while True:
-        try:
-            auto1688.main(commodity_list)
-        except Exception as a:
-            continue
+    MyThread('4723', '9', '8SUKKBYDZTS8N7B6').start()
+    MyThread('4724', '12', 'VAUBB24423011293').start()
